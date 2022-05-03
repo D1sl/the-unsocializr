1722bf9a19d46bfc187ce9482276b70cbec5509f
D1sl <benjaminmolini@gmail.com>
2022-05-02 20:08:11 -0500

    Thought model

---
+++ models/Thought.js
@@ -0,0 +1,57 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+    }
+)
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+    },
+    {
+        toJSON: {}
+    }
+)
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.reduce((total, reaction) => total + reaction.length + 1, 0)
+});
+
+module.exports = Thought;
