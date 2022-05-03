39d1ea76c1e9cb246deb1f98683b4e9d9d19b640
D1sl <94911312+D1sl@users.noreply.github.com>
2022-05-03 11:56:26 -0500

    add friends

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -52,6 +52,21 @@ const userController = {
         User.findOneAndDelete({ _id: params.id })
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.json(err))
+    },
+
+    addFriend({ params }, res) {
+        User.findOneAndUpdate(
+            { _id: params.id },
+            { $push: { friends: params.friendId } },
+        )
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id!' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.json(err))
     }
 }
 
