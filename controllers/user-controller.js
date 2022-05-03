4d56d369621df139e060085d0cb0d448b8f781d5
D1sl <benjaminmolini@gmail.com>
2022-05-02 20:17:38 -0500

    Adds API routes for USER and controller to fetch all users

---
+++ controllers/user-controller.js
@@ -0,0 +1,18 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+        .populate({
+            path: 'friends',
+            select: '-__v'
+        })
+        .select('-__v')
+        .sort({ _id: -1 })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
+        });
+    }
+}
